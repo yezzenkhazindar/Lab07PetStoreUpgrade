f3effb5a2b1fc0e9029c27daaecce1b3cb5ebff9
Emilie <98420041+themilkprince@users.noreply.github.com>
2022-06-23 14:11:07 -0600

    Create Bird.java

---
+++ src/Bird.java
@@ -0,0 +1,16 @@
+public class Bird extends Pets {
+    public Bird(String name, String breed){
+        super(name, breed);
+    }
+
+    //TODO put your override for makeNoise here
+
+    //TODO put your overload for makeNoise here
+
+    //this method returns "[name] ate some seeds", where [name] is the Class variable name
+    public String eatSeeds(){
+        return getName() + "ate some seeds";
+    }
+
+    //TODO put your overloads for eatSeeds() here
+}
