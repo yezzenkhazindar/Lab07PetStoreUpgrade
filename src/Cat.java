5094a45ffa555ab3bb50af62c5b04cde859e29de
yezzenkhazindar <122915516+yezzenkhazindar@users.noreply.github.com>
2023-04-21 11:49:15 -0600

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -1,1 +1,22 @@
-//Paste your Cat.java from Lab04 here
+
+public class Cat extends Pets {
+    String badName;
+    public Cat(String name, String breed, String badName){
+        super(name, breed);
+        this.badName = badName;
+    }
+
+    @Override
+    public String makeNoise() {
+        return "meow";
+    }
+
+    @Override
+    public String getName(){
+        return name + " aka " + badName;
+    }
+
+    public static void main(String[] args){
+
+    }
+}
